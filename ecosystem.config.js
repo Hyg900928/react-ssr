6fa1e4e434d27020795aa0d20e29dd2308a8cd49
贺运高 <1040083796@qq.com>
2018-12-03 12:36:32 +0800

    hhhh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
       repo: "git@github.com:Hyg900928/react-ssr.git",
       path: "/root/www/react-ssr/production",
       ssh_options: "StrictHostKeyChecking=no",
-      "post-deploy": ' git pull origin master && npm install && npm run build-prod && npm run start-prod --env production',
+      "post-deploy": ' git pull origin master && cnpm install && npm run build-prod && npm run start-prod --env production',
       env: {
         NODE_ENV: 'production'
       }
