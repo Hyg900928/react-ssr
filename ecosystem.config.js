0a02e33212728fc2d7be18772b394fbcf49dfc58
贺运高 <1040083796@qq.com>
2018-12-05 10:13:23 +0800

    mm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -26,6 +26,9 @@ module.exports = {
       repo: "git@github.com:Hyg900928/react-ssr.git",
       path: "/root/www/react-ssr/production",
       ssh_options: "StrictHostKeyChecking=no",
+      "pre-setup": "ls -la",
+      "post-setup": "ls -la",
+      "pre-deploy-local": "echo 'This is a local executed command'",
       "post-deploy": 'yarn && yarn run build-prod && yarn run start-prod',
       env: {
         NODE_ENV: 'production'
